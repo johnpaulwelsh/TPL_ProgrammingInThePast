649abcf4c0245f6d9e33b9f338039396edb8fe87
John Paul Welsh <wearing2raincoats@yahoo.com>
2014-02-02 18:19:22 -0500

    added decrypt, same limitations on encrypt functionality (no wrapping, ...)

--- fortran/cypher.f95
+++ fortran/cypher.f95
@@ -13,8 +13,10 @@ input_string='JOHN PAUL WELSH'
 shift_amt=3
 output_string=''
 
-call encrypt(input_string, string_pos, shift_amt)
 !return output_string and print it
+call encrypt(input_string, string_pos, shift_amt)
+
+call decrypt(input_string, string_pos, shift_amt)
 
 end program cypher
 
@@ -41,7 +43,7 @@ do string_pos=1,15
     
     output_string(string_pos:string_pos)=achar(code)
     
-    print *, 'After shift'
+    print *, 'After encrypt shift'
     print *, 'Char = ', ch, ', code = ', code
     print *
 end do
@@ -49,4 +51,37 @@ end do
 print *, output_string
 
 end subroutine encrypt
+!++++++++++++++++++++++++++++++
+
+!++++++++++++++++++++++++++++++
+subroutine decrypt(input_string, string_pos, shift_amt)
+implicit none
+
+character :: input_string*15, output_string*15, ch*1
+integer :: string_pos, code, shift_amt, shift_count
+
+do string_pos=1,15
+
+    ch=input_string(string_pos:string_pos)
+    code=iachar(ch)
+    
+    print *, 'Char = ', ch, ', code = ', code
+
+    !do not shift whitespace
+    if (code .NE. 32) then
+        do shift_count=1, shift_amt
+            code=code-1
+        end do
+    end if
+    
+    output_string(string_pos:string_pos)=achar(code)
+    
+    print *, 'After decrypt shift'
+    print *, 'Char = ', ch, ', code = ', code
+    print *
+end do
+
+print *, output_string
+
+end subroutine decrypt
 !++++++++++++++++++++++++++++++
