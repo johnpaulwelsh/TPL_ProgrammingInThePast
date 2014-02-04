a54e4415f34cd47a0752912d4c53cd6523be9cf8
John Paul Welsh <wearing2raincoats@yahoo.com>
2014-02-04 01:49:27 -0500

    solve function works, fixed off-by-one error in all 3 functions

--- fortran/cypher.f95
+++ fortran/cypher.f95
@@ -1,27 +1,27 @@
 !Caesar Cypher
 !Author: John Paul Welsh
 
-!codes are 65..90 for A..Z
-
 program cypher
+
 implicit none
 
-character :: input_string*15, output_string*15
-integer :: string_pos, shift_amt
+character :: input_string*15
+integer :: shift_amt, max_shift_value
 
 input_string='JOHN PAUL WELSH'
-shift_amt=3
-output_string=''
+shift_amt=12
+max_shift_value=26
 
-!return output_string and print it
-call encrypt(input_string, string_pos, shift_amt)
+call encrypt(input_string, shift_amt)
 
-call decrypt(input_string, string_pos, shift_amt)
+call decrypt(input_string, shift_amt)
+
+call solve(input_string, max_shift_value)
 
 end program cypher
 
 !++++++++++++++++++++++++++++++
-subroutine encrypt(input_string, string_pos, shift_amt)
+subroutine encrypt(input_string, shift_amt)
 implicit none
 
 character :: input_string*15, output_string*15, ch*1
@@ -32,15 +32,12 @@ do string_pos=1,15
     ch=input_string(string_pos:string_pos)
     code=iachar(ch)
     
-    print *, 'Char = ', ch, ', code = ', code
-
-    !do not shift whitespace
     if (code .NE. 32) then
         do shift_count=1, shift_amt
             code=code+1
         end do
         if (code > 90) then
-            code=65+MOD(code,90)
+            code=64+MOD(code,90)
         end if
         if (code < 65) then
             code=code-MOD(65,code)
@@ -48,10 +45,6 @@ do string_pos=1,15
     end if
     
     output_string(string_pos:string_pos)=achar(code)
-    
-    print *, 'After encrypt shift'
-    print *, 'Char = ', ch, ', code = ', code
-    print *
 end do
 
 print *, output_string
@@ -60,7 +53,7 @@ end subroutine encrypt
 !++++++++++++++++++++++++++++++
 
 !++++++++++++++++++++++++++++++
-subroutine decrypt(input_string, string_pos, shift_amt)
+subroutine decrypt(input_string, shift_amt)
 implicit none
 
 character :: input_string*15, output_string*15, ch*1
@@ -71,32 +64,60 @@ do string_pos=1,15
     ch=input_string(string_pos:string_pos)
     code=iachar(ch)
     
-    print *, 'Char = ', ch, ', code = ', code
-
-    !do not shift whitespace
     if (code .NE. 32) then
         do shift_count=1, shift_amt
             code=code-1
         end do
         if (code > 90) then
-            code=65+MOD(code,90)
+            code=64+MOD(code,90)
         end if
         if (code < 65) then
-            print *, 'Mod time... ', MOD(65,code)
-            code=91-MOD(65,code)
-            print *, 'New code is ', code
-            
+            code=91-MOD(65,code)           
         end if
     end if
     
     output_string(string_pos:string_pos)=achar(code)
-    
-    print *, 'After decrypt shift'
-    print *, 'Char = ', output_string(string_pos:string_pos), ', code = ', code
-    print *
 end do
 
 print *, output_string
 
 end subroutine decrypt
+!++++++++++++++++++++++++++++++
+
+!++++++++++++++++++++++++++++++
+subroutine solve(input_string, max_shift_value)
+implicit none
+
+character :: input_string*15, output_string*15, ch*1
+integer :: string_pos, code, max_shift_value, shift_count, shift_amt
+integer :: iter_count
+
+do iter_count=max_shift_value,0,-1
+
+    shift_amt=iter_count
+
+    do string_pos=1,15
+
+        ch=input_string(string_pos:string_pos)
+        code=iachar(ch)
+    
+        if (code .NE. 32) then
+            do shift_count=1, shift_amt
+                code=code+1
+            end do
+            if (code > 90) then
+                code=64+MOD(code,90)
+            end if
+            if (code < 65) then
+                code=91-MOD(65,code)            
+            end if
+        end if
+
+        output_string(string_pos:string_pos)=achar(code)
+    end do
+    
+    print *, 'Caesar ',iter_count,': ',output_string
+end do
+
+end subroutine solve
 !++++++++++++++++++++++++++++++
