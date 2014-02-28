a75b584ce5829beeb75b6c52a674648c7c25274f
johnpaulwelsh <wearing2raincoats@yahoo.com>
2014-02-28 18:03:04 -0500

    finished scala (pending unforseen revisions), copy-pasted a Hello World into the .cobol file because why not

--- scala-procedural/cipher.scala
+++ scala-procedural/cipher.scala
@@ -1,14 +1,18 @@
 object Cipher {
     def encrypt(inputStr: String, shiftAmt: Int):String = {
-        var outputStr:String = ""
+        var outputStr = ""
         var newCh = ' '
         var newCode = 0
 
         for(chCount <- 0 until 15) {
             newCh = inputStr.charAt(chCount)
             newCode = newCh.asInstanceOf[Int]
-            newCode = newCode + shiftAmt
-            newCh = newCode.asInstanceOf[Char]
+            if (newCode != 32) {
+                newCode = newCode + shiftAmt
+                if (newCode > 90) newCode = 64 + (newCode % 90)
+                if (newCode < 65) newCode = 91 - (65 % newCode)
+                newCh = newCode.asInstanceOf[Char]
+            }
             outputStr = outputStr + newCh
         }
 
@@ -16,19 +20,47 @@ object Cipher {
     }
 
     def decrypt(inputStr: String, shiftAmt: Int):String = {
-        var outputStr:String = ""
+        var outputStr = ""
         var newCh = ' '
         var newCode = 0
 
         for(chCount <- 0 until 15) {
             newCh = inputStr.charAt(chCount)
             newCode = newCh.asInstanceOf[Int]
-            newCode = newCode - shiftAmt
-            newCh = newCode.asInstanceOf[Char]
+            if (newCode != 32) {
+                newCode = newCode - shiftAmt
+                if (newCode > 90) newCode = 64 + (newCode % 90)
+                if (newCode < 65) newCode = 91 - (65 % newCode)
+                newCh = newCode.asInstanceOf[Char]
+            }
             outputStr = outputStr + newCh
         }
 
         return outputStr
+    }
+
+    def solve(inputStr: String, maxShiftVal: Int) {
+        var shift = 0
+        var newCh = ' '
+        var newCode = 0
+
+        for(iteration <- maxShiftVal to 0 by -1) {
+            var outputStr = ""
+            shift = iteration
+            
+            for(chCount <- 0 until 15) {
+                newCh = inputStr.charAt(chCount)
+                newCode = newCh.asInstanceOf[Int]
+                if (newCode != 32) {
+                    newCode = newCode - shift
+                    if (newCode > 90) newCode = 64 + (newCode % 90)
+                    if (newCode < 65) newCode = 91 - (65 % newCode)
+                    newCh = newCode.asInstanceOf[Char]
+                }
+                outputStr = outputStr + newCh
+            }
+            println("Caesar " + iteration + ": " + outputStr)
+        }
     }
 
     def main(args: Array[String]) {
@@ -43,6 +75,6 @@ object Cipher {
         println("The maximum shift value for solve is " + maxShiftVal)
         println("Encrypted: " + encrypt(inputStr, shiftAmt))
         println("Decrypted: " + decrypt(inputStr, shiftAmt))
-        //solve(inputStr, maxShiftVal))
+        solve(inputStr, maxShiftVal)
     }
 }
