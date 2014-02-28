54cdafdabf352bcbc572fb0232c62794a219556c
johnpaulwelsh <wearing2raincoats@yahoo.com>
2014-02-28 17:09:00 -0500

    finally getting somewhere with Scala: encrypt/decrypt work with no bounds checking

--- scala-procedural/cipher.scala
+++ scala-procedural/cipher.scala
@@ -1,29 +1,48 @@
-object Cypher {
+object Cipher {
+    def encrypt(inputStr: String, shiftAmt: Int):String = {
+        var outputStr:String = ""
+        var newCh = ' '
+        var newCode = 0
 
-    def encrypt(inputStr: String, shiftAmt: Integer): String = {
-        var outputStr = inputStr
-        var len = inputStr.length()
-        var chCount = 0
-        var newCh, newCode
-        
-        for (chCount <- 0 until len) {
+        for(chCount <- 0 until 15) {
             newCh = inputStr.charAt(chCount)
-            newCode = newCh
+            newCode = newCh.asInstanceOf[Int]
             newCode = newCode + shiftAmt
-            newCh = newCode
-            outPutStr.charAt(chCount) = newCh
+            newCh = newCode.asInstanceOf[Char]
+            outputStr = outputStr + newCh
         }
-        
+
+        return outputStr
+    }
+
+    def decrypt(inputStr: String, shiftAmt: Int):String = {
+        var outputStr:String = ""
+        var newCh = ' '
+        var newCode = 0
+
+        for(chCount <- 0 until 15) {
+            newCh = inputStr.charAt(chCount)
+            newCode = newCh.asInstanceOf[Int]
+            newCode = newCode - shiftAmt
+            newCh = newCode.asInstanceOf[Char]
+            outputStr = outputStr + newCh
+        }
+
         return outputStr
     }
 
     def main(args: Array[String]) {
         var inputStr = "John Paul Welsh"
-        var shiftAmt = 15
+        var shiftAmt = 1
         var maxShiftVal = 26
         
         inputStr = inputStr.toUpperCase()
 
-        println(encrypt(inputStr, shiftAmt))
+        println("The input string is " + inputStr)
+        println("The shift amount for encrypt and decrypt is " + shiftAmt)
+        println("The maximum shift value for solve is " + maxShiftVal)
+        println("Encrypted: " + encrypt(inputStr, shiftAmt))
+        println("Decrypted: " + decrypt(inputStr, shiftAmt))
+        //solve(inputStr, maxShiftVal))
     }
 }
